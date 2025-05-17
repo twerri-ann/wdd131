04e0522d67b7a024311e124bbd0b41405d58d1c5
twerri-ann <terriannbassaragh491@gmail.com>
2025-05-17 11:50:25 -0500

    added files and updated some

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,22 +1,25 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('bom.html');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector('#list'); 
+
 button.addEventListener('click', function () {
     if (input.value.trim() !== '') {
-
         const li = document.createElement('li');
         li.textContent = input.value.trim();
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+        });
+
+        li.appendChild(deleteButton);
         list.appendChild(li);
+
         input.value = '';
         input.focus();
     } else {
-        alert("Please enter a chapter name.")
+        alert("Please enter a chapter name.");
         input.focus();
     }
 });
