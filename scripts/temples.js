8fad864a791fb847bda9be315edbed197e4a077a
twerri-ann <terriannbassaragh491@gmail.com>
2025-05-20 18:56:55 -0500

    updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,11 @@
+const toggleButton = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+
 const yearSpan = document.getElementById("year");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
