1019232384877b1f43d8011a4d8bfaad79266edf
twerri-ann <terriannbassaragh491@gmail.com>
2025-05-28 19:34:06 -0500

    saved

---
+++ week04/scripts/objects.js
@@ -0,0 +1,26 @@
+let aCourse = {
+  code: "WDD131",
+  title: "Dynamic Web Fundamentals",
+  credits: 2,
+  sections : [
+    { section: "001", enrolled: 95, instructor: "Roberto Diaz Rodriguez" },
+    { section: "002", enrolled: 80, instructor: "Sarah Gobble" }
+  ]
+};
+
+function setCourseInformation(course) {
+  document.querySelector("#courseName").innerHTML = `${course.code} – ${course.title}`;
+}
+
+function sectionTemplate(section) {
+  return `<tr>
+           <td>${section.sectionNumber}</td>
+           <td>${section.enrolled}</td>
+           <td>${section.instructor}</td>
+          </tr>`;
+}
+
+function renderSections(course) {
+  const html = sections.map(sectionTemplate);
+  document.querySelector("#sections tbody").innerHTML = html.join("");
+}
