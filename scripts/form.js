c2cdeb66b5dd737ce7edfddbad84d9ff9d33dedb
twerri-ann <terriannbassaragh491@gmail.com>
2025-06-07 11:31:09 -0500

    modified

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,14 @@
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
 const yearSpan = document.getElementById("year");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
