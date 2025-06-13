124e68c69289464c11159c50f6f7655f79eb84b1
twerri-ann <terriannbassaragh491@gmail.com>
2025-06-12 21:24:50 -0500

    added and adjusted

--- project/scripts/hcs.js
+++ project/scripts/hcs.js
@@ -1,3 +1,39 @@
+const cuisines = [
+  {
+    name: "Italian"
+  },
+  {
+    name: "Chinese"
+  },
+  {
+    name: "Mexican"
+  },
+  {
+    name: "Indian"
+  },
+  {
+    name: "Thai"
+  },
+  {
+    name: "West African"
+  },
+  {
+    name: "Mediterranean"
+  },
+  {
+    name: "Middle Eastern"
+  },
+  {
+    name: "Caribbean"
+  },
+  {
+    name: "Mixed"
+  },
+  {
+    name: "Others"
+  },
+];
+
 const yearSpan = document.getElementById("year");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
